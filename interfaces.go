b46284fd596d2a95f8b212e0f5f9ace5ef9c47e3
Robert Peters <r2p2.gw@gmail.com>
2017-08-26 22:28:40 +0200

    Rename files to match structure names

---
+++ interfaces.go
@@ -0,0 +1,51 @@
+package r2aft
+
+type LocalNode interface {
+	Id() uint64
+
+	RequestVote(
+		term uint64,
+		candidateId uint64,
+		lastLogIndex uint64,
+		lastLogTerm uint64,
+	) (uint64, error)
+
+	VoteReply(
+		term uint64,
+		err error,
+	)
+
+	AppendEntries(
+		term uint64,
+		leaderId uint64,
+		prevLogIndex uint64,
+		prevLogTerm uint64,
+		entries []Entry,
+		leaderCommit uint64,
+	) (uint64, error)
+
+	AppendEntriesReply(
+		term uint64,
+		err error,
+	)
+}
+
+type RemoteNode interface {
+	Id() uint64
+
+	RequestVote(
+		term uint64,
+		candidateId uint64,
+		lastLogIndex uint64,
+		lastLogTerm uint64,
+	)
+
+	AppendEntries(
+		term uint64,
+		leaderId uint64,
+		prevLogIndex uint64,
+		prevLogTerm uint64,
+		entries []Entry,
+		leaderCommit uint64,
+	)
+}
